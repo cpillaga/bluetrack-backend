78f374d73322035a2aee63fbce5e92730ccf6c63
Christian Pillaga <christian_pillaga_s@hotmail.es>
2021-03-09 12:01:15 -0500

    convenio

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,6 +8,7 @@ app.use(require('./empresa'));
 app.use(require('./sucursal'));
 app.use(require('./usuarios'));
 app.use(require('./cliente'));
+app.use(require('./convenio'));
 app.use(require('./destinatario'));
 
 module.exports = app;
