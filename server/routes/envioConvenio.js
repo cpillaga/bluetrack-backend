7f3dbd5214d4819925d4f94a30f1013d1d194fc5
Christian Pillaga <christian_pillaga_s@hotmail.es>
2021-05-19 10:54:45 -0500

    rastreo

--- server/routes/envioConvenio.js
+++ server/routes/envioConvenio.js
@@ -127,7 +127,7 @@ app.get('/shippingAgreement/rastreo/:idClient/:rastreo', verificaToken, function
                 });
             }
 
-            console.log("id envio: " + shippingAgreementBD._id);
+            console.log("id envio: " + shippingAgreementBD);
             Status.find({ shippingAgreement: shippingAgreementBD._id })
                 .populate('shippingAgreement')
                 .sort({ date: -1 })
